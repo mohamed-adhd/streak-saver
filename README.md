dfe947696f957a1a6ebe9f83810f147311c9f5ee
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-16 19:08:14 +0000

    annnnd its alive , the app now can commit and push automatically,nexft milestone is the front end , then the vercel host

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 vercel hosted app that automatically commits and pushes to github to save a streak
 
 
-repo queued after :
-mhd_sql
+repo in progress
--- app/github/__init__.py
+++ app/github/__init__.py
@@ -1,15 +1,10 @@
-<<<<<<< Updated upstream
-
-streak-saver-bot on 2026-08-16
-=======
 import auth
 import commits
 import client
 
 def main():
     s=client.clinet_on()
-    #print(commits.fetch(s,"mohamed-adhd"))
-    commits.commit(s,"mohamed-adhd","streak-saver","app/github/__init__.py")
+    if (commits.fetch(s,"mohamed-adhd")=="save"):
+        commits.commit(s,"mohamed-adhd","streak-saver","app/github/__init__.py")
 
 main()
->>>>>>> Stashed changes
