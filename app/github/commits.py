cc8c5400cd7af741a08e5fdfb8ea82837c6e12e4
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-16 19:05:06 +0000

    hmm , merge problems

--- app/github/commits.py
+++ app/github/commits.py
@@ -1,19 +1,16 @@
+
 from datetime import datetime
 def fetch(s,username):
     today = datetime.now().strftime("%Y-%m-%d")
-    commits = s.search_commits(query="author:" + username + " committer-date:2026-08-15")
-    if commits is not None:
+    commits = s.search_commits(query="author:" + username + " committer-date:"+today)
+    if commits.totalCount != 0:
         return "safe"
     else:
         return "save"
 
-def commit(s,username,repo,file):
+def commit(s, username, repo, file):
     today = datetime.now().strftime("%Y-%m-%d")
-    rep = s.get_repo("mohamed-adhd/"+repo)
-    file = repo.get_contents(file)
-    new_shi = file.decoded_content.decode("utf-8") +("\nstreak-saver-bot on "+today+"\n")
-    repo.update_file(path=file,message="streak-saver got u ;)",content=new_shi,sha=file.sha,
-    )
-
-
-
+    rep = s.get_repo(username + "/" + repo)
+    content_file = rep.get_contents(file)
+    new_shi = content_file.decoded_content.decode("utf-8") + ("\nstreak-saver-bot on " + today + "\n")
+    rep.update_file(path=file,message="streak-saver got u ;)",content=new_shi,sha=content_file.sha)
