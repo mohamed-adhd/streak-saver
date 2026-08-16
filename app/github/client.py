4d0e6cf8cd42785360d4b5d2a1ccc39a5a7934c3
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-16 18:25:49 +0000

    the app is capable of fetching the commits for an author on a specific day

--- app/github/client.py
+++ app/github/client.py
@@ -1,11 +1,14 @@
 from github import Github
-
+from datetime import datetime
 import auth
 
-def clinet_on ():
+def clinet_on (username):
     g= auth.initiate()
     s=Github(auth=g)
-    print(s.search_repositories(query="language:python"))
+    today = datetime.now().strftime("%Y-%m-%d")
+    commits=s.search_commits(query="author:"+username+" committer-date:2026-08-15")
+    for commit in commits:
+        print(commit)
 
 
-clinet_on()
+clinet_on("mohamed-adhd")
