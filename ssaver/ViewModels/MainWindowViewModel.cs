8eb159285b4b7c7e2bdda10cd51eb1d93deef388
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-17 21:36:19 +0000

    setup page added

--- ssaver/ViewModels/MainWindowViewModel.cs
+++ ssaver/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ssaver.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    [ObservableProperty] private ViewModelBase current_page;
     [RelayCommand]
     public void clik()
     {
-        
+        current_page = new configsViewModel();
     }
     
 }
--- ssaver/Views/configs.axaml.cs
+++ ssaver/Views/configs.axaml.cs
@@ -1,11 +1,24 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace ssaver.Views;
-
-public partial class configs : Window
+namespace StreakSaver.Views
 {
-    public configs()
+    public partial class SetupPage : UserControl
     {
-        InitializeComponent();
+        public SetupPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnVerifyClicked(object? sender, RoutedEventArgs e)
+        {
+            var username = UsernameBox.Text;
+            var repoName = RepoNameBox.Text;
+            var filePath = FilePathBox.Text;
+            var token = TokenBox.Text;
+
+            // TODO: validate the token against the repo (e.g. GitHub API call)
+            // and navigate onward on success.
+        }
     }
 }
