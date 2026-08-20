904e4e935a6222af05e1148c9a8438ff33d7cdd6
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-20 21:04:54 +0000

    deployement is successful , added json handeling jsut for api test, moment of truth

--- app/main.py
+++ app/main.py
@@ -1,3 +1,26 @@
 from fastapi import FastAPI
-
+import app
+from pydantic import BaseModel
+class SetupRequest(BaseModel):
+    username: str
+    repo: str
+    file: str
+    token: str
 app = FastAPI()
+@app.get("/status")
+def status():
+    return {"status": "ok"}
+
+@app.post("/setup")
+def setup(data: SetupRequest):
+    return {
+        "username": data.username,
+        "repo": data.repo
+    }
+@app.get("/save")
+def save():
+    return {"status": "saved"}
+
+@app.get("/")
+def root():
+    return {"message": "Streak-Saver API"}
