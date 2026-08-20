ad18d14004c35db4b2b7b098f062d15a46649a13
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-20 20:19:23 +0000

    vercel didnt quite like the empty vercel.json

--- api/index.py
+++ api/index.py
@@ -1,14 +1,20 @@
 import app
-
-
+from pydantic import BaseModel
+class SetupRequest(BaseModel):
+    username: str
+    repo: str
+    file: str
+    token: str
 @app.get("/status")
 def status():
     return {"status": "ok"}
 
-@app.get("/setup")
-def setup():
-    return {"status": "sucess"}
-
+@app.post("/setup")
+def setup(data: SetupRequest):
+    return {
+        "username": data.username,
+        "repo": data.repo
+    }
 @app.get("/save")
 def save():
     return {"status": "saved"}
