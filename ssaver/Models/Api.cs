02438be123ed26b7e65416c3ae1de6bad15fe26e
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-20 21:15:50 +0000

    added a simple test in the setup app to test the api endpoint

--- ssaver/Models/Api.cs
+++ ssaver/Models/Api.cs
@@ -1,11 +1,14 @@
+using System;
+
 namespace ssaver.Models;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
-using System.Text.Encodings;
+using System.Text;
+using ssaver.Models;
 public class Api
 {
-    async Task<int> send(string username, string repo, string file, string token)
+    public async Task<int> send(string username, string repo, string file, string token)
     {
         HttpClient client = new HttpClient();
         var data = new
@@ -14,7 +17,9 @@ public class Api
         var content = new StringContent(json,Encoding.UTF8,"application/json");
         
         
-        HttpResponseMessage res = await client.PostAsync(url, content);    
-        
+        HttpResponseMessage res = await client.PostAsync("streak-saver-six.vercel.app/status", content);
+        System.Console.WriteLine(res.Content.ReadAsStringAsync().Result);
+        return 1;
+
     }
 }
--- ssaver/ViewModels/configsViewModel.cs
+++ ssaver/ViewModels/configsViewModel.cs
@@ -2,21 +2,22 @@ using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-
+using ssaver.Models;
 namespace ssaver.ViewModels;
 
 
 public partial class configsViewModel : ViewModelBase
 {
     [ObservableProperty] private MainWindowViewModel _main;
+    private Api s; 
     public configsViewModel(MainWindowViewModel main)
     {
         _main = main;
-        
     }
     [RelayCommand]
     private void onVerifyClicked()
     {
+        s.send("verify","s","s","s");
         _main.Current_page = new DoneViewModel();
     }
 }
