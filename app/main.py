87ad412288dbe628f6b93fdd4110c10541cb89f4
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-22 19:13:21 +0000

    added the env storing on post

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import app
+from upstash_redis import redis
+import os
+from cryptography.fernet import Fernet
 from pydantic import BaseModel
 class SetupRequest(BaseModel):
     username: str
@@ -7,12 +10,17 @@ class SetupRequest(BaseModel):
     file: str
     token: str
 app = FastAPI()
-@app.get("/status")
+@app.post("/status")
 def status():
     return {"status": "ok"}
 
 @app.post("/setup")
 def setup(data: SetupRequest):
+    os.environ['TOKEN'] = data.token
+    os.environ['REPO'] = data.repo
+    os.environ['FILE'] = data.file
+    os.environ['USERNAME'] = data.username
+
     return {
         "username": data.username,
         "repo": data.repo
