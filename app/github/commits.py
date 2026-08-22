3f2b854fb92b5437aed46d52a723c6dccae3d1fc
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-22 22:51:02 +0000

    officially , the app is capable of checking the commits and pushing one in danger case based on a schedual , next milestone is polish and closing the repo ,then moving on to the fortran allocator

--- app/github/commits.py
+++ app/github/commits.py
@@ -12,5 +12,3 @@ def commit(s, username, repo, file):
     today = datetime.now().strftime("%Y-%m-%d")
     rep = s.get_repo(username + "/" + repo)
     content_file = rep.get_contents(file)
-    new_shi = "#"+content_file.decoded_content.decode("utf-8") + ("\nstreak-saver-bot on " + today + "\n")
-    rep.update_file(path=file,message="streak-saver got u ;)",content=new_shi,sha=content_file.sha)
