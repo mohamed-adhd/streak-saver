48302c8a404026ea2747b25c81a4afd9338457d2
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-23 12:35:53 +0000

    smtp section done , building the api point for the warning

---
+++ app/streak/checker.py
@@ -0,0 +1,15 @@
+import smtplib
+import os
+from email.message import EmailMessage
+
+msg = EmailMessage()
+msg["Subject"] = "My Nigga get some work done !"
+msg["From"] = "streaksaver.adhd@example.com"
+msg["To"] = "midouamdouni4@gmail.com"
+msg.set_content("dawg its getting late , open arch n get some commits going ,cmon gang get off valorant")
+
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    smtp=os.environ["smtp"]
+    server.login("streaksaver.adhd@example.com",smtp)
+    server.send_message(msg)
